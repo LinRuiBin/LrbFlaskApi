a225c962100b9f1d51152170ac1c4a9d41d9bb39
3gtc <3gtc@3gtcdeMac-mini.local>
2019-04-25 17:22:19 +0800

    增加githook

--- app/web/web.py
+++ app/web/web.py
@@ -1,12 +1,26 @@
 
 from flask import Blueprint
-from flask import request,jsonify,json
+from flask import request,jsonify,json,abort
 
 from app.libs.helper import ops_render
-
+import subprocess
 
 web_route = Blueprint('web', __name__)
 
 @web_route.route("/",methods = [ "GET","POST" ])
 def index():
     return ops_render("common/base.html")
+
+
+#版本更新
+@web_route.route("/githook",methods = [ "GET","POST" ])
+def githook():
+    if request.method == "POST":
+        retcode = subprocess.call("cd /home/FlaskProject/LrbFlaskApi && git checkout . && git pull && /bin/bash uwsgiServer.sh restart")
+        if retcode == 0:
+            return jsonify({"status":"success"},200)
+        else:
+            return jsonify({"status": "error"}, 503)
+
+    else:
+        abort(400)
