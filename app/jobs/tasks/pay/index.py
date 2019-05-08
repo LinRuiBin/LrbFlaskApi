43da74a96a7e4909df7750b8ffbe41880a067f94
3gtc <3gtc@3gtcdeMac-mini.local>
2019-05-08 15:40:02 +0800

    测试定时任务

--- app/jobs/tasks/pay/index.py
+++ app/jobs/tasks/pay/index.py
@@ -32,11 +32,11 @@ def payJob():
         user = User.query.filter_by(nickname='Super').first()
         if user:
             phone = int(user.phone)
-            if phone < 18675079600:
+            if phone < 18675079700:
                 phone += 1
-            if phone > 18675079700:
-                phone -= 10
+            if phone == 18675079700:
+                phone -= 100
             user.phone = str(phone)
             db.session.add(user)
             db.session.commit()
-            # print('自动执行任务完成')
+            print('自动执行任务完成')
