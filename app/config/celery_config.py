0476d600c966e2427be246755b824bc353852351
3gtc <3gtc@3gtcdeMac-mini.local>
2019-05-20 18:10:47 +0800

    测试celery任务3

--- app/config/celery_config.py
+++ app/config/celery_config.py
@@ -13,7 +13,7 @@ task_serializer = 'json'
 result_serializer = 'json'
 accept_content = ['json']
 timezone = "Asia/Shanghai"  # 时区设置
-worker_hijack_root_logger = True  # celery默认开启自己的日志，可关闭自定义日志，不关闭自定义日志输出为空
+worker_hijack_root_logger = False  # celery默认开启自己的日志，可关闭自定义日志，不关闭自定义日志输出为空
 result_expires = 60 * 60 * 24  # 存储结果过期时间（默认1天）
 
 # 导入任务所在文件
@@ -25,7 +25,8 @@ imports = [
 beat_schedule = {
     "test1": {
         "task": "app.celery.tasks.tetstCelery",  #执行的函数
-        "schedule": crontab(minute=0, hour="*/6"),   # every minute 每分钟执行
+        "schedule": crontab(minute=1),
+        # "schedule": crontab(minute=0, hour="*/6"),   # every minute 每分钟执行
         "args": ()  # # 任务函数参数
     },
 }
